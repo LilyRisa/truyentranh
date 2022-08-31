9179f77dcf455764dc981dc431bcdafb62856310
duonghoo <duonghoo99@gmail.com>
2022-08-31 08:26:39 +0700

    d home

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,8 +1,52 @@
 <html>
-  <head>
-    <link rel="typesheet" href="{{asset('css/main.css')}}">
-  </head>
-  <body>
-    <button class="btn btn-danger">test</button>
-  </body>
+
+<head>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg_primary">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a href="/"> <img class="top-bar-logo" src="//static.fanfox.net/v202020210123121/mangafox/images/logo.png"> </a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item ms-3">
+          <a class="nav-link active" aria-current="page" href="#">TRANG CHỦ</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link" href="#">TRUYỆN MỚI</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link" href="#">BXH</a>
+        </li>
+        <li class="nav-item ms-3">
+          <a class="nav-link" href="#">LỌC TRUYỆN</a>
+        </li>
+      </ul>
+      <div class="h-100 align-items-center">
+      <form class="d-flex">
+        <input class="form-control me-2 rounded-pill w-50 h-50" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success w-50 h-50" type="submit">Search</button>
+
+        <div class="top-bar-avatar-con me-2"> <a href="/login/?from=%2f%3ffbclid%3dIwAR3cDf5LZ5rNuu7bHt8fLHFS7b5Xb8oYSJ_ldGCXmlflDeUC-QKgqZcpqUY"> <img class="top-bar-avatar-img" src="//static.fanfox.net/v202020210123121/mangafox/images/top-bar-avatar-img.png "> </a> </div>
+      </form>
+      </div>
+    </div>
+  </div>
+</nav>
+
+  <div class="top-bar">
+    <div class="container"> 
+      <div class="top-bar-list"> <a class="active" href="/">Home</a> <a href="/releases/">Lastest Updates</a> <a href="/ranking/">Ranking</a> <a href="/directory/">Browse</a> </div>
+      
+      <div class="top-bar-right-list">
+        <input class="person-main-right-sortBar-input" name="title" type="text" id="fastsearch" placeholder="Search Manga"></form> <a href="javascript:void(0);" class="fastsearchbt" style="margin-left: -50px;padding-bottom: 5px;"></a> <a class="top-bar-right-list-2" target="_blank" href="http://www.mangazoneapp.com/">Get the App</a> <span class="top-bar-right-list-3-con"> <a class="top-bar-right-list-3" href="/comichistory/">History</a> </span>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
