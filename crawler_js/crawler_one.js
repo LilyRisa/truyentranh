3ab978d3495bb0b1f19973ed46cc0ded2f7a148c
DESKTOP-AV76JFA\minhm <bvminh101299@gmail.com>
2022-10-11 21:55:01 +0700

    sandbox

--- crawler_js/crawler_one.js
+++ crawler_js/crawler_one.js
@@ -42,7 +42,10 @@ if(CONNECT){
     return 0;
 }
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+ });
 const page = await browser.newPage();
 await page.setUserAgent('Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0');
 
