446edfb0468746ab69fdee2ef8cdc66637f27039
duonghoo <duonghoo99@gmail.com>
2022-10-17 09:41:40 +0700

    d fix font

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="vi">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="/css/main.css?ver=2.03">
+  <link rel="stylesheet" href="/css/main.css?ver=2.50">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
     <meta charset="utf-8">
