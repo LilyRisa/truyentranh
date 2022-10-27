06d459227315377bade036a98aef153d1e2a157e
bvminh <bvminh101299@gmail.com>
2022-10-27 14:38:17 +0700

    fix

--- crawler_js/crawler_cron.js
+++ crawler_js/crawler_cron.js
@@ -332,7 +332,7 @@ function request (element) {
   cron.schedule('0 8 * * *', () => {
     (async () => {
         try{
-            await index_main('https://www.nettruyenin.com/tim-truyen/dam-my', 1, true);
+            await index_main('https://www.nettruyenin.com/tim-truyen/dam-my', 1, false);
             process.exit(0);
         }catch(e){
             await writeFile('./log.txt', e.toString());
